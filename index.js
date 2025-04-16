fb14a513d675fdd6f8a3da65dde8799786ac4f84
Dawn <207631160+KeeningDawn@users.noreply.github.com>
2025-04-16 14:22:30 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = function JSFClassic(mod) {
+	let enabled = true;
+	const replaceTypes = new Set([1, 2, 3, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46]);
+
+	mod.dispatch.addDefinition('S_DIALOG', 99, __dirname + '/S_DIALOG.def', true);
+
+	mod.hook('S_DIALOG', 99, (event) => {
+		if (!enabled || !event.buttons.length) return;
+
+		const buttons = event.buttons;
+		for (let i = 0, len = buttons.length; i < len; i++) {
+			if (replaceTypes.has(buttons[i].type)) {
+				buttons[i].type = 33;
+			}
+		}
+		event.type = 1;
+		return true;
+	});
+
+	mod.command.add('jsf', () => {
+		enabled = !enabled;
+		mod.command.message(`Just Spam F is ${enabled ? 'en' : 'dis'}abled.`);
+	});
+};
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Just Spam F (Classic)
+
+Allows you to press "F" for most (if not all) dialog buttons.
+
+This mod is built for patch 31. It *may* work on patch 29, but you'll likely need a custom S_DIALOG definition file compatible with that version.
+
+## Installation
+
+Click the green "Code" button, then select "Download ZIP".
+
+Move the inner folder into your Toolbox `mods` folder.
+
+## Commands
+
+| Command |  Description                |
+| :------ |  :------------------------- |
+| `!jsf` or `/8 jsf` | Toggles the mod. **Enabled** by default. |
